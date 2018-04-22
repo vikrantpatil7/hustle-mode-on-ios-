b509fc9bfe90cfe46464287a04013f744f628674
vikrantpatil7 <vikrant.patil6@gmail.com>
2018-04-22 18:39:09 +0530

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Vikrant  Patil on 4/22/18.
+//  Copyright © 2018 Vikrant  Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
